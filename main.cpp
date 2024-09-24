6599e1eaee65bc16e0faa65b3ddc68816e6c7e33
junios-study <junios@naver.com>
2024-09-24 09:45:22 +0900

    x64 x85 추가

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-#include "Engine.h"
+#include "SDL.h"
 
-int main()
+#pragma comment(lib, "SDL2")
+#pragma comment(lib, "SDL2main")
+
+int SDL_main(int agrc, char* agrv[])
 {
-	GEngine->LoadLevel("Level01.Map");
-
-	GEngine->Run();
 
 	return 0;
 }
+
+
+//#include "Engine.h"
+//
+//int main()
+//{
+//	GEngine->LoadLevel("Level01.Map");
+//
+//	GEngine->Run();
+//
+//	return 0;
+//}
